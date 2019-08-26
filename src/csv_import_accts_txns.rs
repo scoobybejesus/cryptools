1e256c9ebeea4e4a2e6705ac47f60f153aafcbae
scoobybejesus <scoobybejesus@users.noreply.github.com>
2019-08-25 23:42:33 -0400

    Corrected full URL of LEGAL.txt.

--- src/csv_import_accts_txns.rs
+++ src/csv_import_accts_txns.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2017-2019, scoobybejesus
-// Redistributions must include the license: https://github.com/scoobybejesus/cryptools-rs/LEGAL.txt
+// Redistributions must include the license: https://github.com/scoobybejesus/cryptools-rs/blob/master/LEGAL.txt
 
 use std::error::Error;
 use std::process;
