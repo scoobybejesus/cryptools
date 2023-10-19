d2a255ad2e6b34661b2510d9bf0d36abc4d817ad
scoobybejesus <scoobybejesus@users.noreply.github.com>
2023-10-19 17:17:51 -0400

    Change edition. Update copyright year.

--- crptls/src/account.rs
+++ crptls/src/account.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020, scoobybejesus
+// Copyright (c) 2017-2023, scoobybejesus
 // Redistributions must include the license: https://github.com/scoobybejesus/cryptools/blob/master/LEGAL.txt
 
 use std::rc::{Rc, Weak};
