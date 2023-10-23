60ccbfb1ff9cf85dd106c77ed7fa2896143318e6
scoobybejesus <scoobybejesus@users.noreply.github.com>
2023-10-23 18:07:24 -0400

    Changed structopt dependency to clap and updated to latest version.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@
 use std::path::PathBuf;
 use std::error::Error;
 
-use structopt::StructOpt;
+use clap::Parser;
 
 mod setup;
 mod cli_user_choices;
@@ -25,50 +25,50 @@ mod mytui;
 use export::{export_all, export_je};
 
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "cryptools")]
+#[derive(Parser, Debug)]
+#[command(name = "cryptools")]
 pub struct Cli {
 
     /// User is instructing the program to skip the data entry wizard.
     /// When set, default settings will be assumed if they are not set by 
     /// environment variables (or .env file) or certain command line flags.
-    #[structopt(name = "accept args", short = "a", long = "accept")]
+    #[arg(id = "accept args", short, long = "accept")]
     accept_args: bool,
 
     /// Suppresses the printing of "all" reports, except that it *will* trigger the
     /// exporting of a txt file containing an accounting journal entry for every transaction.
     /// Individual account and transaction reports may still be printed via the print_menu
     /// with the -p flag. Note: the journal entries are not suitable for like-kind transactions.
-    #[structopt(name = "journal entries", short, long = "journal-entries")]
+    #[arg(id = "journal entries", short, long = "journal-entries")]
     journal_entries_only: bool,
 
     /// Once the file_to_import has been fully processed, the user will be presented
     /// with a menu for manually selecting which reports to print/export. If this flag is not
     /// set, the program will print/export all available reports.
     #[cfg(feature = "print_menu")]
-    #[structopt(name = "print menu", short, long = "print-menu")]
+    #[arg(id = "print menu", short, long = "print-menu")]
     print_menu: bool,
 
     /// Prevents the program from writing reports to files.
     /// This will be ignored if -a is not set (the wizard will always ask to output).
-    #[structopt(name = "suppress reports", short, long = "suppress")]
+    #[arg(id = "suppress reports", short, long = "suppress")]
     suppress_reports: bool,
 
     /// Output directory for exported reports.
-    #[structopt(name = "output directory", short, long = "output", default_value = ".", parse(from_os_str))]
+    #[arg(id = "output directory", short, long = "output", default_value = ".")]
     output_dir_path: PathBuf,
 
     /// Causes the program to expect the `txDate` field in the file_to_import to use the format YYYY-MM-dd
     /// or YY-MM-dd (or YYYY/MM/dd or YY/MM/dd) instead of the default US-style MM-dd-YYYY or MM-dd-YY 
     /// (or MM/dd/YYYY or MM/dd/YY).
     /// NOTE: this flag overrides the ISO_DATE environment variable, including if set in the .env file.
-    #[structopt(name = "imported file uses ISO 8601 date format", short = "i", long = "iso")]
+    #[arg(id = "imported file uses ISO 8601 date format", short, long = "iso")]
     iso_date: bool,
 
     /// Tells the program a non-default date separator (instead of a hyphen "-", a slash "/") was used
     /// in the file_to_import `txDate` column (i.e. 2017-12-31 instead of 2017/12/31).
     /// NOTE: this flag overrides the DATE_SEPARATOR_IS_SLASH environment variable, including if set in the .env file.
-    #[structopt(name = "date separator character is slash", short = "d", long = "date-separator-is-slash")]
+    #[arg(id = "date separator character is slash", short, long = "date-separator-is-slash")]
     date_separator_is_slash: bool,
 
     /// File to be imported.  Some notes on the columns: (a) by default, the program expects the `txDate` column to 
@@ -77,7 +77,7 @@ pub struct Cli {
     /// separator; and (c) there is now experimental support for negative values being wrapped in parentheses. Use
     /// the python script for sanitizing/converting negative values if they are a problem.
     /// See .env.example for further details on environment variables.
-    #[structopt(name = "file_to_import", parse(from_os_str))]
+    #[arg(id = "file_to_import")]
     file_to_import: Option<PathBuf>,
 }
 
@@ -109,7 +109,7 @@ pub struct Cfg {
 
 fn main() -> Result<(), Box<dyn Error>> {
 
-    let args = Cli::from_args();
+    let args = Cli::parse();
 
     println!(
         "\
